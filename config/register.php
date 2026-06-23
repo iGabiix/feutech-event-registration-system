5e0463dda04f8f441df442b85ec6d45afae773ec
iGabiix <g.gamboa.dev@gmail.com>
2026-06-24 00:00:37 +0800

    Created initial project pages

---
+++ config/register.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Register</title>
+</head>
+<body>
+    <h2>Register Page</h2>
+
+    <form>
+        <input type="text" placeholder="Full Name"><br><br>
+        <input type="email" placeholder="Email"><br><br>
+        <input type="password" placeholder="Password"><br><br>
+        <button type="submit">Register</button>
+    </form>
+</body>
+</html>
